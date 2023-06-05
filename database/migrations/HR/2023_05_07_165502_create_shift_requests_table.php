5194500f9ff2ac4a39609e122d36a23dc4d0c26e
alaa-zamel <alaa.zamel21@gmail.com>
2023-06-05 08:23:48 +0300

    feat: update migration files

--- database/migrations/HR/2023_05_07_165502_create_shift_requests_table.php
+++ database/migrations/HR/2023_05_07_165502_create_shift_requests_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
         Schema::create('shift_requests', function (Blueprint $table) {
             $table->id('shift_req_id');
             $table->unsignedBigInteger('emp_id');
-            $table->unsignedBigInteger('req_stat_id');
+            $table->unsignedBigInteger('req_stat')->default('1');
             $table->text('description');
+            $table->time('new_time_in');
+            $table->time('new_time_out');
+            $table->date('start_date');
+            $table->date('end_date');
 
             $table->foreign('emp_id')->references('emp_id')->on('employees');
-            $table->foreign('req_stat_id')->references('req_stat_id')->on('request_statuses');
-
             $table->timestamps();
         });
     }
---
+++ database/migrations/HR/2023_05_29_121928_create_vacation_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vacation_requests', function (Blueprint $table) {
+            $table->id('vacation_req_id');
+            $table->unsignedBigInteger('emp_id');
+            $table->Integer('req_stat');
+            $table->text('description');
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacation_requests');
+    }
+};
