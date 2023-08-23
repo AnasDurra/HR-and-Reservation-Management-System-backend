7aa15c3bfa8c39699c68e665f0e6620b6f89fbc3
alaa-zamel <alaa.zamel21@gmail.com>
2023-08-23 04:40:20 +0300

    update action seeder

--- database/seeders/ActionSeeder.php
+++ database/seeders/ActionSeeder.php
@@ -14,182 +14,182 @@ class ActionSeeder extends Seeder
     public function run()
     {
 
-        //1
+        //1 *
         Action::create([
             'name' => 'إضافة طلب توظّيف',
             'description' => 'Add new job application',
             'severity' => 1,
         ]);
 
-        //2
+        //2 *
         Action::create([
             'name' => 'قبول طلب توظيف',
             'description' => 'Approve job application',
             'severity' => 1,
         ]);
 
-        //3
+        //3 *
         Action::create([
             'name' => 'رفض طلب توظيف',
             'description' => 'Reject job application',
             'severity' => 1,
         ]);
 
-        //4
+        //4 *
         Action::create([
             'name' => 'تعديل طلب توظيف',
             'description' => 'Edit job application',
             'severity' => 2,
         ]);
 
-        //5
-        Action::create([
-            'name' => 'تعديل سجل موظف',
-            'description' => 'Edit employee record',
-            'severity' => 2,
-        ]);
-
-        //6
-        Action::create([
-            'name' => 'حذف موظَف',
-            'description' => 'Delete employee',
-            'severity' => 3,
-        ]);
-
-        //7
+        //5 *
         Action::create([
             'name' => 'إضافة موَظف',
             'description' => 'Add new employee',
             'severity' => 1,
         ]);
 
-        //8
+        //6 *
+        Action::create([
+            'name' => 'تعديل سجل موظف',
+            'description' => 'Edit employee record',
+            'severity' => 2,
+        ]);
+
+        //7 *
+        Action::create([
+            'name' => 'حذف موظَف',
+            'description' => 'Delete employee',
+            'severity' => 3,
+        ]);
+
+        //8 *
         Action::create([
             'name' => 'تسجيل حضور موظف',
             'description' => 'Log employee attendance',
             'severity' => 2,
         ]);
 
-        //9
+        //9 *
         Action::create([
             'name' => 'تسجيل انصراف موظَف',
             'description' => 'Log employee leaving',
             'severity' => 2,
         ]);
 
-        //10
+        //10 *
         Action::create([
             'name' => 'إضافة طلب إجازة',
             'description' => 'Add vacation request',
             'severity' => 2,
         ]);
 
-        //11
+        //11 *
         Action::create([
             'name' => 'قبول طلب إجازة',
             'description' => 'Approve vacation request',
             'severity' => 1,
         ]);
 
-        //12
+        //12 *
         Action::create([
             'name' => 'رفض طلب إجازة',
             'description' => 'Reject vacation request',
             'severity' => 1,
         ]);
 
-        //13
+        //13 *
         Action::create([
             'name' => 'تعديل طلب إجازة',
             'description' => 'Edit vacation request',
             'severity' => 2,
         ]);
 
-        //14
+        //14 *
         Action::create([
             'name' => 'حذف طلب إجازة',
             'description' => 'Delete vacation request',
             'severity' => 2,
         ]);
 
-        //15
+        //15 *
         Action::create([
             'name' => 'إضافة طلب إزاحة دوام',
             'description' => 'Add shift change request',
             'severity' => 2,
         ]);
 
-        //16
+        //16 *
         Action::create([
             'name' => 'قبول طلب إزاحة دوام',
             'description' => 'Approve shift change request',
             'severity' => 1,
         ]);
 
-        //17
+        //17 *
         Action::create([
             'name' => 'رفض طلب إزاحة دوام',
             'description' => 'Reject shift change request',
             'severity' => 1,
         ]);
 
-        //18
+        //18 *
         Action::create([
             'name' => 'تعديل طلب إزاحة دوام',
             'description' => 'Edit shift change request',
             'severity' => 2,
         ]);
 
-        //19
+        //19 *
         Action::create([
             'name' => 'حذف طلب إزاحة دوام',
             'description' => 'Delete shift change request',
             'severity' => 2,
         ]);
 
-        //20
+        //20 *
         Action::create([
             'name' => 'إنشاء مسمى وظيفي',
             'description' => 'Create job title',
             'severity' => 1,
         ]);
 
-        //21
+        //21 *
         Action::create([
             'name' => 'تعديل صلاحيات مسمى وظيفي (اضافة)',
             'description' => 'Add permissions to job title',
             'severity' => 2,
         ]);
 
-        //22
+        //22 *
         Action::create([
             'name' => 'تعديل صلاحيات مسمى وظيفي (حذف)',
             'description' => 'Remove permissions from job title',
             'severity' => 2,
         ]);
 
-        //23
+        //23 *
         Action::create([
             'name' => 'تعديل صلاحيات مستخدم (اضافة)',
             'description' => 'Add permissions to user',
             'severity' => 2,
         ]);
 
-        //24
+        //24 *
         Action::create([
             'name' => 'تعديل صلاحيات مستخدم (حذف)',
             'description' => 'Remove permissions from user',
             'severity' => 2,
         ]);
 
-        //25
+        //25 *
         Action::create([
             'name' => 'إسناد مسمى وظيفي',
             'description' => 'Assign job title to user',
             'severity' => 1,
         ]);
 
-        //26
+        //26 *
         Action::create([
             'name' => 'تبرير غياب موظف',
             'description' => 'Justify employee absence',
